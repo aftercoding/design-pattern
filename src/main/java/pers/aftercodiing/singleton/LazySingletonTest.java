3a1ed65bccbc6062cf96b23525932eead2658058
sean.wang <sean.wang@ms.com>
2022-09-16 00:01:54 +0800

    add singleton

---
+++ src/main/java/pers/aftercodiing/singleton/LazySingletonTest.java
@@ -0,0 +1,17 @@
+package pers.aftercodiing.singleton;
+
+/**
+ * @description: some desc
+ * @author: congjun
+ * @email: 66@7788.com
+ * @date: 2022-09-15 23:56
+ */
+public class LazySingletonTest {
+    public static void main(String[] args) {
+
+    }
+}
+
+class LazySingleTon{
+
+}
